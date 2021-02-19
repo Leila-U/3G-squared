a914ad2c03392665ff7b2e1ea8d2c947dd954196
Leila-U <leila.uy@mail.utoronto.ca>
2021-02-19 01:07:09 -0500

    Call npm build on react app

---
+++ 3g-squared-client/build/static/js/3.d7334301.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp3g-squared-client"]=this["webpackJsonp3g-squared-client"]||[]).push([[3],{113:function(t,i,s){"use strict";s.r(i);var c=s(1);i.default=function(t){var i=t.src,s=t.width,e=t.height;return Object(c.jsx)("img",{src:"/img/svg/".concat(i),alt:i,with:s,height:e})}}}]);
+//# sourceMappingURL=3.d7334301.chunk.js.map
